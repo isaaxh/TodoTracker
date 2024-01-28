00370c71f37b9077b6426d494d70b9ad9aa67496
Isaac Hussain <isaaclearns58@gmail.com>
2024-01-28 08:18:38 +0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
+import axios from "axios";
+
+function App() {
+  const handleClick = async () => {
+    const response = await axios.get("http://localhost:3333/hello-world");
+    console.log("response: ", response.data);
+  };
+  return (
+    <ChakraProvider>
+      <Box>Hello Chakra</Box>
+      <Button colorScheme="purple" onClick={handleClick}>
+        Click me
+      </Button>
+    </ChakraProvider>
+  );
+}
+export default App;
